cc289f564148a06f66a15a25a1a86604c271e469
Shema-arsene <aniyonkurushema@gmail.com>
2024-12-26 10:35:55 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import About from "./Components/About"
+import Footer from "./Components/Footer"
+import Hero from "./Components/Hero"
+import Reviews from "./Components/Reviews"
+
+const App = () => {
+  return (
+    <main className="">
+      <Hero />
+      <About />
+      <Reviews />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
